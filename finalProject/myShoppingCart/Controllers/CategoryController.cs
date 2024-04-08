05ddf450dca40f18493b64a59f91263c5e3c9026
mobydickhm1851 <mobydickhm1851@gmail.com>
2024-04-08 23:56:23 +0800

    show model data in view

--- finalProject/myShoppingCart/Controllers/CategoryController.cs
+++ finalProject/myShoppingCart/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using myShoppingCart.Models;
 
 namespace myShoppingCart.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly CartContext _db;
+        public CategoryController(CartContext db)
+        {
+            _db= db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
