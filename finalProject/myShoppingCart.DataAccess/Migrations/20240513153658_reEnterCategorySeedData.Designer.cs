60317d24abbb22b96057609ac0085e74d2aad929
mobydickhm1851 <mobydickhm1851@gmail.com>
2024-05-13 23:39:37 +0800

    update database at home computer

--- finalProject/myShoppingCart.DataAccess/Migrations/20240513153658_reEnterCategorySeedData.Designer.cs
+++ finalProject/myShoppingCart.DataAccess/Migrations/20240513153658_reEnterCategorySeedData.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using myShoppingCart.ViewModels;
 
 #nullable disable
 
-namespace myShoppingCart.Migrations
+namespace myShoppingCart.DataAccess.Migrations
 {
     [DbContext(typeof(CartContext))]
-    partial class CartContextModelSnapshot : ModelSnapshot
+    [Migration("20240513153658_reEnterCategorySeedData")]
+    partial class reEnterCategorySeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
